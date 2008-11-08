c11bc006ae24f7849288727e04d09ca3badaf1ac
joxer <kell.92.k@gmail.com>
2008-11-08 15:25:26 +0100

    simple server

--- server.c
+++ server.c
@@ -1,80 +1,59 @@
-/* 
-
-3-11-08 programmazione rete, prova con bind 
-
-   -aggiunto listen
-   -aggiunto accept
-*/
-
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
-#define BACKLOG 15
 
 int main(){
 
-  int sockfd, be, lis, newfd; // socket file descriptor && bind result && listen && new file descriptor
+  struct addrinfo local, *serv;
+  struct sockaddr_storage it;
+  socklen_t addr_size;
+  int addr;
 
-  struct sockaddr_in local, remote ; // local && remote information
+  int sock, bn, ln, acc;
 
+  memset(&local, 0, sizeof local);
 
-  local.sin_family = AF_INET;
-  local.sin_port = htons(6000);
-  local.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(local.sin_zero,"\0" , sizeof local.sin_zero );
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-    
-    fprintf(stderr, "ERROR initialize socket: %s", gai_strerror(sockfd));
+  local.ai_family = AF_UNSPEC;
+  local.ai_socktype = SOCK_STREAM;
+  local.ai_flags = AI_PASSIVE;
+  if((addr = getaddrinfo(NULL, "3490", &local, &serv)) == -1){
+    perror("error");
     exit(1);
   };
 
-  if((be = bind(sockfd, (struct sockaddr *)&local, sizeof local)) == -1){
+  sock = socket(serv->ai_family, serv->ai_socktype, serv->ai_protocol);
+
+  if((bn = bind(sock, serv->ai_addr, serv->ai_addrlen)) == -1){
     
-    fprintf(stderr, "ERROR binding socket: %s", gai_strerror(sockfd));
+    perror("error");
     exit(1);
-
   };
+
+  ln = listen(sock, 10);
   
-  if((lis = listen(sockfd, BACKLOG)) == -1){
+  
+  addr_size = sizeof it;
 
-    fprintf(stderr, "ERROR listening on the socket: %s", gai_strerror(sockfd));
-    exit(1);
+  if((acc = accept(sock, (struct sockaddr *)&it, &addr_size)) == -1){
 
+    perror("accept");
     
-  };
-  
 
-  printf("waiting connection....");
-
-  socklen_t sin_size;
-  sin_size = sizeof remote;
-
-  while(1){
-
-    if((newfd = accept(sockfd, (struct sockaddr *)&remote, sizeof remote)) == -1){
-      perror("accept");
-      
-      continue;
-
-
-    }
-    if(send(newfd, "ciao mondo\n", 10, 0) == -1){
-
-      perror("send");
-      
-    };
-    printf("ACCEPTED");
+  }else{
+    
+    puts("CONNECTED");
+    
 
   };
   
-  close(newfd);
-  puts("ALL COMPLETE");
+  puts("LULZ");
 
-  return 0;
+  
+
+
+  
 }
